867ba37dc4c8cd36fd3f22b83a8a4018ec0e011a
Andrew Sverdrup <sverdra1@my.erau.edu>
2017-04-01 17:37:31 -0700

    added js and test code

---
+++ binaryTree.js
@@ -0,0 +1,9 @@
+var x = 1000
+function myFunction(p1, p2) {
+    var tmp = p1 * p2;              // The function returns the product of p1 and p2
+    document.getElementById("output").value = tmp;
+}
+
+
+
+document.getElementById("output").value = x;
