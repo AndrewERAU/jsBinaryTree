4ff02c5879a876d50d6a6534d1dd345230f1cf99
Andrew Sverdrup <sverdra1@my.erau.edu>
2017-04-01 19:29:15 -0700

    added initial binary tree code and test code

--- binaryTree.js
+++ binaryTree.js
@@ -3,8 +3,49 @@ function myFunction() {
     var tmp = document.getElementById("userNum").value * document.getElementById("userNum").value;
     document.getElementById("output").innerHTML = tmp;
 }
-function init() {
-   document.getElementById("output").innerHTML = x;
+
+// function init() {
+//    document.getElementById("output").innerHTML = x;
+// }
+
+//window.onload = init;
+
+
+/*function Apple (type) {
+    this.type = type;
+    this.color = "red";
+    this.getInfo = function() {
+        return this.color + ' ' + this.type + ' apple';
+    };
+}*/
+// Start
+function Node (data) {
+    this.data = data;
+    this.leftChild = null;
+    this.rightChild = null;
+    this.outStr = "";
 }
 
-window.onload = init;
+function BinaryTree() {
+   this.root = null;
+
+   this.visit = function(node) {
+      this.outStr += " " + node.data;
+   }
+   this.preorder = function (node) {
+      this.visit(node);
+      this.preorder(node.leftChild);
+      this.preorder(node.rightChild);
+
+      // show traveral string
+      document.getElementById("output").innerHTML = this.outStr;
+   }
+}
+
+function test() {
+   var tree = new BinaryTree();
+   tree.root = new Node(5);
+   tree.root.leftChild = new Node(2);
+   tree.root.rightChild = new Node(102);
+   tree.preorder(tree.root);
+}
